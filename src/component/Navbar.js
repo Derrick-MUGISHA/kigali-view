3285d1e7a8491b797a56fdfaf409265b66cad951
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2025-01-29 10:43:28 +0200

    preess

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,13 +3,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faHeart, faBars } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookF, faInstagram, faTwitter, faTiktok, faLinkedinIn, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import './Navbar.css';
+import 'font-awesome/css/font-awesome.min.css';
+import { Si1And1 } from 'react-icons/si';
 
 const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);    
-    const [isSticky, setIsSticky] = useState(false); 
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [isSticky, setIsSticky] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false); // New state for dropdown
+    const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen); // Function to toggle dropdown
 
     const toggleMobileMenu = () => {
-        setMenuOpen(!menuOpen); 
+        setMenuOpen(!menuOpen);
     };
 
     const handleScroll = () => {
@@ -50,24 +54,91 @@ const Navbar = () => {
                         <li className="nav-item">
                             <a href="/">Meeting Planners</a>
                             <div className="dropdown-menu">
-                                <a href="/">Event Option 1</a>
-                                <a href="/">Event Option 2</a>
-                                <a href="/">Event Option 3</a>
-                                <a href="/">Event Option 4</a>
-                                <a href="/">Event Option 5</a>
-                                <a href="/">Event Option 6</a>
-                                <a href="/">Event Option 7</a>
-                                <a href="/">Event Option 8</a>
-                                <a href="/">Event Option 9</a>
-                                <a href="/">Event Option 10</a>
+                                <a href="/">Meetings</a>
+
+                                <div className="dropdown">
+                                    <a href="/" className="dropdown-toggle">
+                                        Why Kigali <i className="fa fa-caret-right"></i>
+                                    </a>
+
+                                    <div className="dropdown-content">
+                                        <a href="/">Top 10 reasons to chose kIGALI </a>
+                                        <a href="/">What's New In KIGALI</a>
+                                        <a href="/">Trading Blog</a>
+                                        <a href="/">Travel and Transportation In Kigali</a>
+                                        <a href="/">District</a>
+                                        <a href="/">HOTELS</a>
+                                        <a href="/">Parks</a>
+                                        <a href="/">R</a>
+                                    </div>
+                                </div>
+
+                                <div className="dropdown">
+                                    <a href="/" className="dropdown-toggle">
+                                        Planning Tool Kit <i className="fa fa-caret-right"></i>
+                                    </a>
+
+                                    <div className="dropdown-content">
+                                        <a href="/">Service Provider</a>
+                                        <a href="/">Venue</a>
+                                        <a href="/">Housing</a>
+                                        <a href="/">Staffing</a>
+                                        <a href="/">Green Meetings</a>
+                                        <a href="/">Social responsibility</a>
+                                        <a href="/">Security</a>
+                                        <a href="/">Corporate Events</a>
+                                    </div>
+                                </div>
+
+                                <div className="dropdown">
+                                    <a href="/" className="dropdown-toggle">
+                                        Promo Resources <i className="fa fa-caret-right"></i>
+                                    </a>
+
+                                    <div className="dropdown-content">
+                                        <a href="/">Kigali City Guide</a>
+                                        <a href="/">Kigali Attractions</a>
+                                        <a href="/">Kigali Restaurants</a>
+                                        <a href="/">Kigali Events</a>
+                                        <a href="/">Kigali Nightlife</a>
+                                    </div>
+                                </div>
+
+                                <a href="/">Contact Us</a>
+                                <a href="/">Submit your RFP (Request for Proposal)</a>
                             </div>
                         </li>
                         <li className="nav-item">
                             <a href="/">Tour & Reunion Planners</a>
                             <div className="dropdown-menu">
-                                <a href="/">Option 1</a>
-                                <a href="/">Option 2</a>
-                                <a href="/">Option 3</a>
+                                <div className="dropdown">
+                                    <a href="/" className="dropdown-toggle">
+                                        Group Tour<i className="fa fa-caret-right"></i>
+                                    </a>
+
+                                    <div className="dropdown-content">
+                                        <a href="/">Kigali City Tour</a>
+                                        <a href="/">Kigali Group Activities</a>
+                                        <a href="/">Kigali Historical Sites</a>
+                                        <a href="/">Kigali Sightseeing</a>
+                                        <a href="/">Kigali Tour Packages</a>
+                                    </div>
+                                </div>
+                                <a href="/">Reunion</a>
+                                <div className="dropdown">
+                                    <a href="/" className="dropdown-toggle">
+                                        Itineraries <i className="fa fa-caret-right"></i>
+                                    </a>
+
+                                    <div className="dropdown-content">
+                                        <a href="/">Kigali 1-Day Itinerary</a>
+                                        <a href="/">Kigali 3-Day Itinerary</a>
+                                        <a href="/">Kigali 7-Day Itinerary</a>
+                                    </div>
+                                </div>
+                                <a href="/">Experimentor Tours</a>
+                                <a href="/">Accessibility Guide</a>
+                                <a href="/">Video</a>
                             </div>
                         </li>
                         <li className="nav-item">
@@ -124,65 +195,74 @@ const Navbar = () => {
                         <li className="nav-item-2">
                             <a href="/">EVENTS</a>
                             <div className="dropdown-menu">
-                                <a href="/">Event Option 1</a>
-                                <a href="/">Event Option 2</a>
-                                <a href="/">Event Option 3</a>
-                                <a href="/">Event Option 4</a>
-                                <a href="/">Event Option 5</a>
-                                <a href="/">Event Option 6</a>
-                                <a href="/">Event Option 7</a>
-                                <a href="/">Event Option 8</a>
-                                <a href="/">Event Option 9</a>
-                                <a href="/">Event Option 10</a>
+                                <a href="/">Kigali Music Festival</a>
+                                <a href="/">Kigali International Film Festival</a>
+                                <a href="/">Kigali Cultural Fair</a>
+                                <a href="/">Kigali Marathon</a>
+                                <a href="/">Kigali Food Festival</a>
+                                <a href="/">Kigali Art Exhibition</a>
+                                <a href="/">Kigali Night Market</a>
+                                <a href="/">Kigali Fashion Show</a>
+                                <a href="/">Kigali Tech Conference</a>
+                                <a href="/">Kigali Wine Tasting Event</a>
                             </div>
                         </li>
                         <li className="nav-item-2">
                             <a href="/">THINGS TO DO</a>
                             <div className="dropdown-menu">
-                                <a href="/">Option 1</a>
-                                <a href="/">Option 2</a>
-                                <a href="/">Option 3</a>
+                                <a href="/">Visit Kigali Genocide Memorial</a>
+                                <a href="/">Explore Nyamirambo</a>
+                                <a href="/">Kigali City Tour</a>
+                                <a href="/">Hike Mount Kigali</a>
+                                <a href="/">Visit Inema Art Center</a>
+                                <a href="/">Kigali Convention Center</a>
                             </div>
                         </li>
                         <li className="nav-item-2">
                             <a href="/">RESTAURANTS</a>
                             <div className="dropdown-menu">
-                                <a href="/">Option 1</a>
-                                <a href="/">Option 2</a>
-                                <a href="/">Option 3</a>
-                                <a href="/">Option 4</a>
-                                <a href="/">Option 5</a>
+                                <a href="/">The Hut Restaurant</a>
+                                <a href="/">Brachetto Restaurant</a>
+                                <a href="/">Chez Robert</a>
+                                <a href="/">Fusion Restaurant & Lounge</a>
+                                <a href="/">Repub Lounge</a>
+                                <a href="/">Kigali Serena Hotel Restaurant</a>
+                                <a href="/">Bourbon Coffee</a>
+                                <a href="/">Zen Restaurant</a>
                             </div>
                         </li>
                         <li className="nav-item-2">
                             <a href="/">HOTELS</a>
                             <div className="dropdown-menu">
-                                <a href="/">Option 1</a>
-                                <a href="/">Option 2</a>
-                                <a href="/">Option 3</a>
-                                <a href="/">Option 4</a>
-                                <a href="/">Option 5</a>
-                                <a href="/">Option 6</a>
+                                <a href="/">Kigali Serena Hotel</a>
+                                <a href="/">Radisson Blu Hotel Kigali</a>
+                                <a href="/">The Manor Hotel</a>
+                                <a href="/">Hotel des Mille Collines</a>
+                                <a href="/">Amani Boutique Hotel</a>
+                                <a href="/">Onomo Hotel Kigali</a>
                             </div>
                         </li>
                         <li className="nav-item-2">
                             <a href="/">NEIGHBORHOODS</a>
                             <div className="dropdown-menu">
-                                <a href="/">Option 1</a>
-                                <a href="/">Option 2</a>
-                                <a href="/">Option 3</a>
-                                <a href="/">Option 4</a>
-                                <a href="/">Option 5</a>
+                                <a href="/">Kiyovu</a>
+                                <a href="/">Nyamirambo</a>
+                                <a href="/">Kimihurura</a>
+                                <a href="/">Gikondo</a>
+                                <a href="/">Nyarutarama</a>
+                                <a href="/">Kacyiru</a>
+                                <a href="/">Remera</a>
+                                <a href="/">Biryogo</a>
                             </div>
                         </li>
                         <li className="nav-item-2">
-                            <a href="/">EXPLORE COLUMBUS</a>
+                            <a href="/">EXPLORE Kigali</a>
                             <div className="dropdown-menu">
-                                <a href="/">Option 1</a>
-                                <a href="/">Option 2</a>
-                                <a href="/">Option 3</a>
-                                <a href="/">Option 4</a>
-                                <a href="/">Option 5</a>
+                                <a href="/">Mount Kigali Hike</a>
+                                <a href="/">Kigali Convention Center</a>
+                                <a href="/">Kigali National Museum</a>
+                                <a href="/">Kigali's Art Galleries</a>
+                                <a href="/">Kigali Nightlife</a>
                             </div>
                         </li>
                     </ul>
